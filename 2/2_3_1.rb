a781a416f9df9f3d2f9c5a0106954e93b9207a82
kotomi1338 <kotomin1216@gmail.com>
2019-07-12 00:24:31 +0900

    2_3_1.rb & 2_3_2.rb

---
+++ 2/2_3_1.rb
@@ -0,0 +1,20 @@
+puts 'シングルクォート'
+puts "ダブルクォート"
+
+puts "こんにちは\nさようなら"
+puts 'こんにちは\nさようなら'
+
+name = 'kotomi'
+puts "Hello, #{name}!"
+i = 10
+puts "#{i}は16進数にすると#{i.to_s(16)}です"
+
+puts 'Hello, #{name}!'
+
+puts 'Hello, ' + name + '!'
+
+puts "こんにちは\\nさようなら"
+puts "Hello, \#{name}!"
+
+puts 'He said, "Don\'t speak."'
+puts "He said, \"Don't speak.\""
